2a40471388133bcd0c53961242bbc86e67407cdf
Floyd Price <f.price@floydprice.com>
2016-03-07 15:06:35 +0000

    lets Meteor Cast

--- app/home/components/Home.jsx
+++ app/home/components/Home.jsx
@@ -6,40 +6,42 @@ let twitterHandle = (name) => {
   return `@${name.replace(" ", "").toLowerCase()}`
 }
 
-const Home = ({tweets}) => (
+class Home extends React.Component{
+  render(){
+    return (
+      <div className="tweets">
+        {
+          this.props.tweets.map((tweet) => {
+            return (
+              <div className="tweet" key={tweet._id}>
+                <div className="avatar">
+                  <img src={tweet.tweeter.avatar} alt={tweet.tweeter.name}/>
+                </div>
 
-  <div className="tweets">
-      {
-        tweets.map((tweet) => {
-          return (
-            <div className="tweet" key={tweet._id}>
-              <div className="avatar">
-                <img src={tweet.tweeter.avatar} alt={tweet.tweeter.name}/>
+                <div className="content">
+                  <div className="header">
+                    <p>{tweet.tweeter.name} <span className="value">{twitterHandle(tweet.tweeter.name)}</span></p>
+
+                    <p><span className="value">{moment(tweet.created_at).fromNow()}</span></p>
+                  </div>
+                  <div className="main">
+                    <p>{tweet.content}</p>
+                  </div>
+                  <div className="meta">
+
+                    <p><i className="fa fa-heart"> </i> <span className="value">{tweet.likes}</span></p>
+                    <p><i className="fa fa-retweet"> </i> <span className="value">{tweet.retweets}</span></p>
+                  </div>
+
+                </div>
               </div>
-
-              <div className="content">
-                <div className="header">
-                  <p>{tweet.tweeter.name} <span className="value">{twitterHandle(tweet.tweeter.name)}</span></p>
-
-                  <p><span className="value">{moment(tweet.created_at).fromNow()}</span></p>
-                </div>
-                <div className="main">
-                  <p>{tweet.content}</p>
-                </div>
-                <div className="meta">
-
-                  <p><i className="fa fa-heart"> </i> <span className="value">{tweet.likes}</span></p>
-                  <p><i className="fa fa-retweet"> </i> <span className="value">{tweet.retweets}</span></p>
-                </div>
-
-              </div>
-            </div>
-          )
-        })
-      }
-  </div>
-
-)
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
 
 
 export default Home
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,33 +1,13 @@
 import Tweets from '../lib/tweets'
-
-let insertNewTweet = (randomDates = false) => {
-  let newTweet = {
-    content: faker.hacker.phrase(),
-    created_at: randomDates ? faker.date.recent(2) : new Date(),
-    likes: faker.random.number(10),
-    retweets: faker.random.number(4),
-    tweeter: {
-      name: faker.name.findName(),
-      avatar: faker.internet.avatar()
-    }
-  }
-  console.log(`inserting new tweet from ${newTweet.tweeter.name}`)
-  Tweets.insert(newTweet)
-}
-
-let insertNewTweets = (times, randomDates = false) => {
-  _.times(times, () => {
-    insertNewTweet(randomDates)
-  })
-}
+import {insertNewTweet, insertNewTweets} from './tweet_factory'
 
 Meteor.startup(() => {
-  // Bootstrap the app with some data
+
   Tweets.remove({})
   insertNewTweets(25, true)
 
-  // Now simulate data coming in...
   Meteor.setInterval(() => {
     insertNewTweets(3)
   }, 7000)
+
 })
